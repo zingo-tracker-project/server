ce4d1f4dea1d783d2eabf66d57d3273ed5079e0e
bigboyang <bigboyang@naver.com>
2025-05-16 21:53:33 +0900

    feat: swagger

--- src/main/java/com/tracker/server/config/CustomConfiguration.java
+++ src/main/java/com/tracker/server/config/CustomConfiguration.java
@@ -1,6 +1,11 @@
 package com.tracker.server.config;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
@@ -17,4 +22,27 @@ public class CustomConfiguration {
         return new JPAQueryFactory(entityManager);
     }
 
+    // localhost:8080/swagger-ui/index.html
+    @Bean
+    public OpenAPI openAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+    private Info apiInfo() {
+        return new Info()
+                .title("API List")
+                .description("오늘도 화이팅")
+                .version("0.1.0");
+    }
 }
